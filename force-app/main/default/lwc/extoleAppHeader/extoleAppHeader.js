949d4ee67f81989adf1557e2bb99951a1e42a6db
Christopher Duskin <cduskin@extole.com>
2026-05-06 13:42:38 -0700

    Refactor to use extole-apex-api-library classes
    - Vendor all library classes into force-app/main/default/classes
    - ExtoleUniversalHandler: replace inline HttpRequest/HttpResponse with
      ExtoleEventFireRequest + ExtoleClient; add withDataMap() usage
    - ExtoleBackfillQueueable: replace person lookup with ExtoleGetPersonRequest;
      shareable calls remain inline (not yet in library)
    - Fix pre-existing test bug: ExtoleBackfillControllerTest.getReports() -> getReports('')

---
+++ force-app/main/default/lwc/extoleAppHeader/extoleAppHeader.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import EXTOLE_LOGO from '@salesforce/resourceUrl/extole_logo';
+
+export default class ExtoleAppHeader extends LightningElement {
+    extoleLogoUrl = EXTOLE_LOGO;
+}
